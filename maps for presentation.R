8989e5dcc791467747d03f3ddb5f03e18f377f62
HRatuainen <heidi_rautiainen@live.se>
2025-01-11 22:58:27 +0100

    commit

--- maps for presentation.R
+++ maps for presentation.R
@@ -1,6 +1,57 @@
 ### maps for presentations
 
 
+# WORLD MAPS
+
+# Load packages
+pcks <- list("dplyr", 
+             "raster", # used for raster data
+             "terra", # used for raster data 
+             "sf", # used for handling sppatial data (e.g. setting coordinate ref system)
+             "ggspatial",
+             "tidyverse")
+
+sapply(pcks, require, char = TRUE) 
+library(rnaturalearth)
+
+
+
+world <- ne_countries(scale = 10)
+                       # country = c("Sweden"))
+# Check projection
+sf::st_crs(world)$proj4string #  unprojected
+world <-  st_transform(world, crs ="+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+
+world <-  st_transform(world, crs ="+proj=laea +x_0=0 + y_0=0 +lon_0=0 +lat_0=0")
+
+
+## plot it 
+
+library(ggspatial)
+
+ggplot() +
+  
+  # plot sweden and lakes 
+  geom_sf(aes(geometry = world$geometry)) + # geom_sf() for vector data 
+  
+  geom_sf(data = world,
+          aes(geometry = world$geometry),
+          fill= "antiquewhite")+
+  
+  
+  
+
+  # add grid lines 
+  theme(panel.grid.major = element_line(color = gray(.5), 
+                                        linetype = "dashed", 
+                                        linewidth  = 0.1))
+
+
+ggsave("output/figx.png", width = 8, height = 6, dpi = 400)
+
+#### RAST VECT
+
+
 library(raster)
 library(geodata)
 library(dplyr)
